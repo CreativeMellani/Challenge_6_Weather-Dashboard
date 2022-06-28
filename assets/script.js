7e6f4bbba95d65dc9efe9aab6808447b56567bfd
CreativeMellani <101590432+CreativeMellani@users.noreply.github.com>
2022-06-27 17:49:08 -0700

    linking weather api in JS.

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
+
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
 const button = document.querySelector('.subButton')
 const inputValue = document.querySelector('.inputvalue')
 const name = document.querySelector('.name')
 const description = document.querySelector('.description')
 const temperature = document.querySelector('.temperature')
 
-
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-
+fetch('
+https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={c40f0425127ac871d43429862b4eda07}')
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
